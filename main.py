5f7cb3b6e31534584f3a0523b9e7bd1bf5795432
fgtrz <hossain.anjir@icloud.com>
2024-01-02 20:46:43 -0800

    chore: refactor identity into iam/

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import base64, os
-from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordBearer
-from rfcllm.core import Prompter as prompter
+from typing import Annotated, Any
+import base64, os, requests
+from fastapi import Depends, FastAPI
+from flask import jsonify
+from rfcllm.config.settings import RFCEP, DTEP 
+from rfcllm.core import Prompter as prompter, RFCRetriever as rfcretriever
+from rfcllm.dto.DocumentMetaDTO import DocumentMetaDTO
+from rfcllm.dto.InquiryDTO import InquiryDTO
 from fastapi.middleware.cors import CORSMiddleware
+from rfcllm.dto.SearchRequestDTO import SearchRequestDTO
 from rfcllm.services.RFCService import Retriever
 from openai import OpenAI
+from rfcllm.utils.validators import is_url
 from rfcllm.iam.routes import iam
-from rfcllm.search.routes import search
-from rfcllm.group.routes import group
-from rfcllm.qa.routes import qa
+from rfcllm.iam.dto import User 
+from rfcllm.iam.utils import get_current_active_user 
 
 OPENAI_API_KEY = base64.b64decode(os.environ.get("OPENAI_API_KEY", ""))
 client = OpenAI(
@@ -21,17 +26,11 @@ prompter = prompter.Prompter()
 
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 retriever = Retriever()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI()
-
-
-origins = ["*"]
+origins = ["http://localhost:5174"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -41,8 +40,73 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# register routes
 app = iam(app)
-app = search(app)
-app = group(app)
-app = qa(app)
+
+@app.post("/search/query/ietf")
+async def search_ietf(search: SearchRequestDTO):
+    print(search)
+    res = retriever.retrieve_search_rfceietf(query=search.query)
+    return {"results": res}
+
+
+@app.post("/search/rfc")
+async def search_rfc(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    search: SearchRequestDTO,
+):
+    rfcid = search.dict()["query"]
+    return {
+        "result": requests.get(f"{RFCEP}{rfcid}.txt").text,
+        "current_user": current_user,
+    }
+
+@app.post("/group/groupmenu")
+async def group_groupmenu(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    print(f'{DTEP}/group/groupmenu.json') 
+    res = requests.get(f'{DTEP}group/groupmenu.json').json()
+    print(res)
+    return {"result": res} 
+
+@app.post("/qa/single/contigious")
+async def qa_single_contigious(
+    current_user: Annotated[User, Depends(get_current_active_user)], inquiry: InquiryDTO
+):
+    # Example of an OpenAI ChatCompletion request
+    # https://platform.openai.com/docs/guides/chat
+    inquiry_as_dic: Any = inquiry.dict()
+    query = inquiry_as_dic["query"]
+    context = inquiry_as_dic["context"]
+
+    if not query or not context:
+        return {"message": "Malformed completion request"}, 401
+
+    res = []
+    try:
+        # construct the whole long prompt by splitting the contents
+        # of the rfc document present at the provided url
+        url = "" if not is_url(context) else context
+        ref_text_meta = (
+            DocumentMetaDTO(**requests.get(url.replace("txt", "json")).json()) or ""
+        )
+        p = prompter.construct_prompt(query, ref_text_meta)
+        ctx = rfcretriever.RFCRetriever(url=url.replace("txt", "html")).load()
+        message: Any = prompter.construct_message(p, ctx)
+        # send a ChatCompletion request to count to 100
+        completion = client.chat.completions.create(
+            model="gpt-4-1106-preview",
+            messages=message,
+        )
+
+        res.append(completion)
+
+        return {
+            "completion": completion,
+            "results": res,
+            "query": query,
+            "context": context,
+            "current_user": current_user,
+        }
+    except requests.exceptions.RequestException as e:
+        return {"message": jsonify({"error": e})}
---
+++ rfcllm/iam/routes.py
@@ -0,0 +1,40 @@
+from datetime import timedelta
+from typing import Annotated, Any
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from rfcllm.iam.dto import Token, User
+from rfcllm.iam.utils import users_db, authenticate_user, create_access_token, get_current_active_user
+
+def iam(app: Any):
+    @app.post("/token", response_model=Token)
+    async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+    ):
+        user = authenticate_user(users_db, form_data.username, form_data.password)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        access_token_expires = timedelta(minutes=30)
+        access_token = create_access_token(
+            data={"sub": user.username}, expires_delta=access_token_expires
+        )
+        return {"access_token": access_token, "token_type": "bearer"}
+
+
+    @app.get("/users/me/", response_model=User)
+    async def read_users_me(
+        current_user: Annotated[User, Depends(get_current_active_user)]
+    ):
+        return current_user
+
+
+    @app.get("/users/me/items/")
+    async def read_own_items(
+        current_user: Annotated[User, Depends(get_current_active_user)]
+    ):
+        return [{"item_id": "Foo", "owner": current_user.username}]
+    
+    return app
